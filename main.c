3c47f778ab5310968b25236e2ebf454215588d3b
jabelic <hibiki990523@gmail.com>
2020-11-24 14:00:13 +0900

    tmp: 2d

--- main.c
+++ main.c
@@ -8,11 +8,11 @@ int main(void){
   int road; // roadはセル数。配列の要素数を変数にするための変数。
   printf("セル数 road=");
   scanf("%d", &road);
-  int r1[2][road], tmp1[road]; // セル数がroad個の配列。
+  int r1[2][road], tmp[2][road]; // セル数がroad個の配列。
 
   for (i = 0; i < road; i++){
     r1[0][i] = 0;
-    tmp1[i] = 0;
+    tmp[0][i] = 0;
   }
 
   int car;
@@ -53,33 +53,33 @@ int main(void){
     for (x = 0; x < road; x++){
       if (x < road - 1){
         if (r1[0][x] == 1 && r1[0][x + 1] == 0){
-          tmp1[x] = 0;
-          tmp1[x + 1] = 1;
+          tmp[0][x] = 0;
+          tmp[0][x + 1] = 1;
           if (x < road - 2){  //右に出すぎてオーバーしちゃうから調節
             x = x + 1;
           }
         }
         else{
-          tmp1[x] = r1[0][x];
+          tmp[0][x] = r1[0][x];
         }
       }else{
         if (r1[0][road - 1] == 1 && r1[0][0] == 0){ //右端１かつ左端０
-          tmp1[road - 1] = 0;
-          tmp1[0] = 1;
+          tmp[0][road - 1] = 0;
+          tmp[0][0] = 1;
         }
         else if (r1[0][road - 1] == 1 && r1[0][0] == 1){
-          tmp1[x] = 1; // x = road - 1
+          tmp[0][x] = 1; // x = road - 1
         }
       }
     }
 
     for (x = 0; x < road; x++){
-      r1[0][x] = tmp1[x];
+      r1[0][x] = tmp[0][x];
     }
 
     for (x = 0; x < road; x++){
       printf("%d", r1[0][x]);
-      tmp1[x] = 0;
+      tmp[0][x] = 0;
     }
 
     p++;
