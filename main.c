fbd185133f34f18710592e248660d088f62c509d
jabelic <hibiki990523@gmail.com>
2020-11-24 16:38:49 +0900

    dimention: 3*road

--- main.c
+++ main.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#define LINE 3
+
 int main(void){
   int i, x, p, t, m;
 
   int road; // roadはセル数。配列の要素数を変数にするための変数。
   printf("セル数 road=");
   scanf("%d", &road);
-  int r1[2][road], tmp[2][road]; // セル数がroad個の配列。
+  int r1[LINE][road], tmp[LINE][road]; // セル数がroad個の配列。
 
 
   // initialization
-  for (i = 0; i < 2; i++){
-    for (j = 0; j < road; j++){
+  for (int i = 0; i < LINE; i++){
+    for (int j = 0; j < road; j++){
       r1[i][j] = 0;
       tmp[i][j] = 0;
     }
   }
 
-  int car;
+  int car, car_num;
   printf("道にいる車の数 car=");
   scanf("%d", &car);
 
@@ -30,17 +32,25 @@ int main(void){
 
   srand((unsigned int)time(NULL)); //実行時間で乱数生成
 
-  while (car > 0){ //セルに車を入れる。
-    x = rand() % road;
+  for(int t = 0; t < LINE; t++){
+    car_num = car;
+    while (car_num > 0){ //セルに車を入れる。
+      x = rand() % road;
 
-    if (r1[0][x] == 0){ //そのセルに元々車がいるか判定
-      r1[0][x] = 1;
-      car = car - 1;
+      if (r1[t][x] == 0){ //そのセルに元々車がいるか判定
+        r1[t][x] = 1;
+        car_num -= 1;
+      }
     }
   }
 
-  for (i = 0; i < road; i++){
-    printf("%d", r1[0][i]);
+  for (int tmp = 0; tmp < LINE; tmp++){
+    for (i = 0; i < road; i++){
+      printf("%d", r1[0][i]);
+    }
+    if (tmp != LINE-1){
+      printf("\n");
+    }
   }
   printf(" /t=0");
   printf("\n");
@@ -52,6 +62,7 @@ int main(void){
   scanf("%d", &t);
 
   p = 0;
+
   while (p < t){
 
     for (x = 0; x < road; x++){
@@ -81,13 +92,17 @@ int main(void){
       r1[0][x] = tmp[0][x];
     }
 
-    for (x = 0; x < road; x++){
-      printf("%d", r1[0][x]);
-      tmp[0][x] = 0;
+    for (int t = 0; t < LINE; t++){
+      for (x = 0; x < road; x++){
+        printf("%d", r1[t][x]);
+        tmp[t][x] = 0;
+      }
+      if (t!=LINE-1){
+        printf("\n");
+      }
     }
-
     p++;
-    printf(" /t=%d\n", p);
+    printf(" /t=%d\n\n", p);
   }
 
   printf("\n");
